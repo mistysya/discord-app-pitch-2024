4dd47d16622d680ce93eb59578feba5e65658bf4
mistysya <ban0214tw@gmail.com>
2024-04-14 12:52:16 +0800

    feat: Combine unity webgl application and discord sdk

---
+++ client/vite.config.ts
@@ -0,0 +1,20 @@
+import {defineConfig} from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  envDir: '../../',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:13001',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+    hmr: {
+      host: 'localhost',
+      protocol: "ws",
+    },
+  },
+});
